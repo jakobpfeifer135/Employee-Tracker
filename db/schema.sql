b51281b2979e3f780a9185e6735c105b2ac5bc5e
jakobpfeifer135 <138628595+jakobpfeifer135@users.noreply.github.com>
2023-10-22 19:34:31 -0500

    added basic starter code using the mini project to base my code upon

---
+++ db/schema.sql
@@ -0,0 +1,19 @@
+
+
+
+CREATE TABLE instructors (
+  id INT NOT NULL,
+  first_name VARCHAR(30),
+  last_name VARCHAR(30),
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE courses (
+  id INT,
+  course_title VARCHAR(30) NOT NULL,
+  instructor_id INT,
+  order_details TEXT,
+  FOREIGN KEY (instructor_id)
+  REFERENCES instructors(id)
+  ON DELETE SET NULL
+);
