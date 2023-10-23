4450dedc4c8107a1ff13a4bba16afae57c31103c
jakobpfeifer135 <138628595+jakobpfeifer135@users.noreply.github.com>
2023-10-22 19:49:11 -0500

    added basic tables into my schema file for future development of app

--- db/schema.sql
+++ db/schema.sql
@@ -1,19 +1,21 @@
-
-
-
-CREATE TABLE instructors (
-  id INT NOT NULL,
-  first_name VARCHAR(30),
-  last_name VARCHAR(30),
-  PRIMARY KEY (id)
+CREATE TABLE department (
+    id INT,
+    name: VARCHAR(30)
 );
 
-CREATE TABLE courses (
-  id INT,
-  course_title VARCHAR(30) NOT NULL,
-  instructor_id INT,
-  order_details TEXT,
-  FOREIGN KEY (instructor_id)
-  REFERENCES instructors(id)
-  ON DELETE SET NULL
+CREATE TABLE role (
+    id INT PRIMARY KEY,
+    title: VARCHAR(30),
+    salary: DECIMAL,
+    department_id: INT
+);
+
+
+CREATE TABLE employee (
+
+id: INT PRIMARY KEY,
+first_name: VARCHAR(30),
+last_name: VARCHAR(30),
+role_id: INT,
+manager_id: INT 
 );
