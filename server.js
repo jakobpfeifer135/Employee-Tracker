4df5149b8e16010026ead6ac5223be6a3ff72721
jakobpfeifer135 <138628595+jakobpfeifer135@users.noreply.github.com>
2023-10-25 16:05:11 -0500

    added new server.js code to display roles for a department when the user selects them from the list of options

--- server.js
+++ server.js
@@ -1,33 +1,60 @@
 
-const mysql = require ('mysql2');
+const mysql = require('mysql2');
 const inquirer = require('inquirer');
 
 const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'imacodernow@126422$',
-    database: ''
-},
-    console.log('Connected')
-);
+  host: 'localhost',
+  user: 'root',
+  password: 'imacodernow@126422$',
+  database: 'employee_db', 
+});
 
-const questions = [
-    {
-      type: 'list',
-      name: 'employee_db',
-      message: 'which department would you like to view',
-      choices: ['Sales','Finance','Engineering','Logistics','Legal','Human Resources']
-    },
-]
+db.connect((err) => {
+  if (err) throw err;
+  console.log('Connected to MySQL database');
+});
 
-inquirer.prompt(questions)
-.then((answers) => {
-    if (answers.employee_db === 'Sales') {
-        const Sales = [{ 
-            type: 'list',
-            name: 'roles',
-            message: 'select a role from within the selected department',
-            choices: ['Sales Manager','']
-        }]
-    }
-})
+const departmentQuestion = {
+  type: 'list',
+  name: 'department',
+  message: 'Which department would you like to view?',
+  choices: ['Sales', 'Finance', 'Engineering', 'Logistics', 'Legal', 'Human Resources'],
+};
+
+inquirer
+  .prompt(departmentQuestion)
+  .then((answers) => {
+    const department = answers.department;
+
+    // Now, based on the department selection, we will present a list of roles
+    // related to that department.
+    const roleQuery = 'SELECT title FROM role WHERE department_id = (SELECT id FROM department WHERE name = ?)';
+
+    db.query(roleQuery, [department], (err, results) => {
+      if (err) {
+        throw err;
+      }
+
+      const roleChoices = results.map((row) => row.title);
+
+      if (roleChoices.length === 0) {
+        console.log('No roles found for the selected department.');
+        db.end();
+      } else {
+        inquirer
+          .prompt([
+            {
+              type: 'list',
+              name: 'role',
+              message: 'Select a role from the selected department:',
+              choices: roleChoices,
+            },
+          ])
+          .then((roleAnswers) => {
+            const selectedRole = roleAnswers.role;
+            console.log('You selected role:', selectedRole);
+            db.end();
+          });
+      }
+    });
+  });
